a73799e3388ef1ac6594d97348181ac32de681e7
TeamwebUnivaq <alessandro.sallese@student.univaq.it>
2020-11-05 17:51:13 +0100

    Insert loginPage button

--- app/src/main/java/org/apereo/cas/casAndroid/LoginActivity.java
+++ app/src/main/java/org/apereo/cas/casAndroid/LoginActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class QrCodeActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qr_code);
+        setContentView(R.layout.activity_login);
     }
 }
