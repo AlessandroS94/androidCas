60d96406828f0a61a65e27600c68b89a4f54cee8
TeamwebUnivaq <alessandro.sallese@student.univaq.it>
2020-11-05 17:23:37 +0100

    Insert QRCODE reader

---
+++ app/src/main/java/org/apereo/cas/casAndroid/QrCodeActivity.java
@@ -0,0 +1,14 @@
+package org.apereo.cas.casAndroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QrCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code);
+    }
+}
